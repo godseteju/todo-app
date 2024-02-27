1da7387d5ca897c26087c53a0c75c1dcd89754db
godseteju <99330973+godseteju@users.noreply.github.com>
2024-02-27 22:16:09 +0530

    pushed todo project

---
+++ resources/views/todos/edit.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel 10 Custom Login and Registration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+          <!-- <a class="navbar-brand" href="#">Navbar</a> -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('todos.create') }}">Todos App</a>
+              </li>
+            </ul>
+            <form action="{{ route('logout') }}" method="POST" class="d-flex" role="search">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">Logout</button>
+            </form>
+          </div>
+        </div>
+    </nav>
+    <br>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Todos App
+                    </div>
+                    <div class="card-body">
+                        <h4> Edit Form </h4>
+                    <form method="post" action="{{ route('todos.update') }}">
+                        @csrf
+                        @method('PUT')
+                        <input type="hidden" name="todo_id" value="{{ $todo->id }}">
+  <div class="mb-3">
+    <label class="form-label">Title</label>
+    <input type="text" class="form-control" id="title" name="title" value="{{ $todo->title }}">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Description</label>
+    <textarea class="form-control" id="description" name="description" cols="5" rows="5">
+        {{ $todo->description }}
+    </textarea>
+  </div>
+    <div class="mb-3">
+    <label for="">Status</label>
+    <select name="is_completed" class="form-control" required="required">
+        <option disabled selected>Select Option</option>
+        <option value="1">Done</option>
+        <option value="0">In Progress</option>
+    </select>
+    </div>
+  
+  <button type="submit" class="btn btn-primary">Update</button>
+</form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
